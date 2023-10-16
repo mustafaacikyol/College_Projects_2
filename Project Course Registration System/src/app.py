4f2d891c3c135561f9e63bdf7906b47c8db5ff6f
Mustafa Açıkyol <acikyolmustafa17@gmail.com>
2023-10-17 00:02:11 +0300

    third commit

--- Project Course Registration System/src/app.py
+++ Project Course Registration System/src/app.py
@@ -23,13 +23,13 @@ class StartApp:
         custom_font = ("Helvetica", 12, "bold")
 
         # Create a button widget to open a new window
-        admin_button = tk.Button(self.window, text="Admin Panel", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge", command=self.open_admin_window)
+        admin_button = tk.Button(self.window, text="Admin Log In", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge", command=self.open_admin_login)
         admin_button.place(relx=0.5, rely=0.4, anchor='center')
 
-        instructor_button = tk.Button(self.window, text="Instructor Panel", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge", command=self.open_instructor_window)
+        instructor_button = tk.Button(self.window, text="Instructor Log In", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge", command=self.open_instructor_login)
         instructor_button.place(relx=0.5, rely=0.5, anchor='center')
 
-        student_button = tk.Button(self.window, text="Student Panel", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge", command=self.open_student_window)
+        student_button = tk.Button(self.window, text="Student Log In", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge", command=self.open_student_login)
         student_button.place(relx=0.5, rely=0.6, anchor='center')
 
         label = tk.Label(self.window, text="Project Course Registration System", font=("Helvetica", 25, "bold"), fg="brown", pady=100)
@@ -37,21 +37,19 @@ class StartApp:
 
         # Add widgets and logic here
 
-    def open_admin_window(self):
-        new_window = tk.Toplevel(self.window)
-        new_window.title("Admin Panel")
-        new_window.state("zoomed")
-        # Add content and widgets for the admin panel here
+    def open_admin_login(self):
+        instance = Admin()
+        instance.login()
 
-    def open_instructor_window(self):
+    def open_instructor_login(self):
         new_window = tk.Toplevel(self.window)
-        new_window.title("Instructor Panel")
+        new_window.title("Instructor Log In")
         new_window.state("zoomed")
         # Add content and widgets for the instructor panel here
 
-    def open_student_window(self):
+    def open_student_login(self):
         new_window = tk.Toplevel(self.window)
-        new_window.title("Student Panel")
+        new_window.title("Student Log In")
         new_window.state("zoomed")
         # Add content and widgets for the student panel here
 
@@ -59,7 +57,43 @@ class StartApp:
         # Start the tkinter main loop
         self.window.mainloop()
     
-    
+class Admin():
+    def __init__(self):
+        self.login_window = tk.Tk()
+
+    def login(self):
+        
+        self.login_window.title("Admin Log In")
+        self.login_window.state("zoomed")
+
+        # Add content and widgets for the admin panel here
+        # Create a Label widget to display text
+        username_label = tk.Label(self.login_window, text="Username : ", font=("Helvetica", 15, "bold"), fg="brown")
+        username_label.place(relx=0.35, rely=0.4)
+
+        password_label = tk.Label(self.login_window, text="Password : ", font=("Helvetica", 15, "bold"), fg="brown")
+        password_label.place(relx=0.35, rely=0.5)
+
+        # Create an Entry widget for text input
+        username_field = tk.Entry(self.login_window, width=20, font=('Arial 15'))
+        username_field.place(relx=0.45, rely=0.4)
+        username_input = username_field.get()
+
+        password_field = tk.Entry(self.login_window, width=20, font=('Arial 15'), show="*")
+        password_field.place(relx=0.45, rely=0.5)
+        password_input = password_field.get()
+
+        submit_button = tk.Button(self.login_window, text="Submit", bg="#99FFFF", fg="#994C00", padx=15, pady=4, font=("Helvetica", 12, "bold"), borderwidth=5, relief="ridge", command=self.close_login)
+        submit_button.place(relx=0.49, rely=0.6)
+
+    def close_login(self):
+        self.login_window.destroy()
+        Admin.dashboard()
+
+    def dashboard():
+        window = tk.Toplevel()
+        window.title("Admin Dashboard")
+        window.state("zoomed")
 
 # Creating an instance of the StartApp class and starting the application
 app = StartApp()
