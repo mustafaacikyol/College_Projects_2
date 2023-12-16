8727d33d29a598f50cf8dfd1d0ba7a3a43ed15aa
Mustafa Açıkyol <acikyolmustafa17@gmail.com>
2023-12-16 15:51:01 +0300

    saturday third dev

--- Restaurant Management System/src/app.py
+++ Restaurant Management System/src/app.py
@@ -15,19 +15,88 @@ class StartApp:
         custom_font = ("Helvetica", 12, "bold")
 
         # Create buttons
-        first_problem_button = tk.Button(self.window, text="First Problem", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge")
+        first_problem_button = tk.Button(self.window, text="First Problem", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge", command=self.open_first_problem)
         first_problem_button.place(relx=0.5, rely=0.4, anchor='center')
 
-        second_problem_button = tk.Button(self.window, text="Second Problem", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge")
+        second_problem_button = tk.Button(self.window, text="Second Problem", bg="#99FFFF", fg="#994C00", padx=20, pady=10, font=custom_font, borderwidth=5, relief="ridge", command=self.open_second_problem)
         second_problem_button.place(relx=0.5, rely=0.53, anchor='center')
 
         label = tk.Label(self.window, text="Restaurant Management System", font=("Helvetica", 25, "bold"), fg="brown", pady=100)
         label.pack()
 
     def run(self):
-
         # Start the tkinter main loop
         self.window.mainloop()
+
+    def open_first_problem(self):
+        first_problem = FirstProblem()
+        first_problem.set_step_number()
+    
+    def open_second_problem(self):
+        second_problem = SecondProblem()
+
+
+class FirstProblem:
+    def __init__(self):
+        # Create a main window
+        self.first_problem_window = tk.Tk()
+
+        # setting attribute
+        self.first_problem_window.state('zoomed')
+
+        # Set the title of the window
+        self.first_problem_window.title("First Problem")
+    
+    def set_step_number(self):
+        self.step_number_label = tk.Label(self.first_problem_window, text="Enter the number of steps : ", font=("Helvetica", 15, "bold"), fg="brown")
+        self.step_number_label.place(relx=0.35, rely=0.4)
+
+        # Create an Entry widget for text input
+        self.step_number_field = tk.Entry(self.first_problem_window, width=3, font=('Arial 15'))
+        self.step_number_field.place(relx=0.55, rely=0.4)
+        
+        self.set_step_number_btn = tk.Button(self.first_problem_window, text="Submit", bg="#99FFFF", fg="#994C00", padx=15, pady=4, font=("Helvetica", 12, "bold"), borderwidth=5, relief="ridge", command=self.open_scenario)
+        self.set_step_number_btn.place(relx=0.43, rely=0.5)
+        
+    def open_scenario(self):
+        step_number = int(self.step_number_field.get())
+        self.first_problem_window.destroy()
+        self.scenario_window = tk.Toplevel()
+        self.scenario_window.state('zoomed')
+        self.scenario_window.title("First Problem")
+
+        y = 0.1
+        for step in range(step_number):
+            step_label = tk.Label(self.scenario_window, text=f"Step {step + 1} : ", font=("Helvetica", 15, "bold"), fg="brown")
+            step_label.place(relx=0.3, rely=y)
+
+            self.customer_field = tk.Entry(self.scenario_window, width=3, font=('Arial 15'))
+            self.customer_field.place(relx=0.4, rely=y)
+            
+            customer_label = tk.Label(self.scenario_window, text=" Customer", font=("Helvetica", 15, "bold"))
+            customer_label.place(relx=0.43, rely=y)
+
+            self.priority_field = tk.Entry(self.scenario_window, width=3, font=('Arial 15'), show="*")
+            self.priority_field.place(relx=0.58, rely=y)
+
+            priority_label = tk.Label(self.scenario_window, text=" Prioritized", font=("Helvetica", 15, "bold"))
+            priority_label.place(relx=0.61, rely=y)
+
+            y+=0.1
+        
+        self.start_app_btn = tk.Button(self.scenario_window, text="Start", bg="#99FFFF", fg="#994C00", padx=15, pady=4, font=("Helvetica", 12, "bold"), borderwidth=5, relief="ridge", command=self.open_scenario)
+        self.start_app_btn.place(relx=0.47, rely=y)
+
+class SecondProblem:
+    def __init__(self):
+        # Create a main window
+        self.second_problem_window = tk.Tk()
+
+        # setting attribute
+        self.second_problem_window.state('zoomed')
+
+        # Set the title of the window
+        self.second_problem_window.title("Second Problem")
 
 # Creating an instance of the StartApp class and starting the application
 app = StartApp()
